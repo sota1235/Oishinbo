6835c62752cf5f8c8333574b2f7c1d29ad6fd23d
ktarow <ktarou569@gmail.com>
2015-08-13 15:24:22 +0000

    add passoword_hash

--- models/account.rb
+++ models/account.rb
@@ -1,6 +1,9 @@
+require "bcrypt"
 
 module Oishinbo
   class Account < ActiveRecord::Base
+    include BCrypt
+
     has_many :account_sections
     has_many :sections, through: :account_sections
 
@@ -12,5 +15,14 @@ module Oishinbo
     validates :password, presence: true
     validates :password, confirmation: true 
     validates :password_confirmation, presence: true
+
+    def password
+      @password ||= Password.new(password_hash)
+    end
+
+    def password=(password)
+      @password = Password.create(password)
+      self.password_hash = @password 
+    end
   end
 end
