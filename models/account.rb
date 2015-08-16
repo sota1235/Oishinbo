0cb83a1c0a766d26633aea79fb27270c03982760
ktarow <ktarou569@gmail.com>
2015-08-16 16:19:15 +0000

    バリデーションのインデントを揃える. パスワードがからの時にエラーメッセージを出力するよう修正

--- models/account.rb
+++ models/account.rb
@@ -9,24 +9,35 @@ module Oishinbo
 
     validates :name, presence: {message: '名前が入力されていません'}
 
-    validates :email, uniqueness: {message: 'このメールアドレスは既に登録されています'}
-    validates :email, presence: {message: 'メールアドレスが入力されていません'}
-    validates :email, format: {
-      with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
-      on: :create ,
-      message: 'メールアドレスの形式が正しくありません'
+    validates :email, {
+      presence: {message: 'メールアドレスが入力されていません'},
+      uniqueness: {message: 'このメールアドレスは既に登録されています'},
+      format: {
+        with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+        on: :create ,
+        message: 'メールアドレスの形式が正しくありません'
+      }
     }
 
-    validates :password, presence: {message: 'パスワードが入力されていません'}
-    validates :password, confirmation: {message: 'パスワード(確認用)が入力されていません'}
-    validates :password_confirmation, presence: {message: 'パスワードが一致しません'}
+    validates :password, {
+      presence: {message: 'パスワードが入力されていません'},
+      confirmation: {message: 'パスワードが一致しません'}
+    }
+
+    validates :password_confirmation, {
+      presence: {message: 'パスワード(確認用)が入力されていません'}
+    }
 
     def password
       @password ||= Password.new(password_hash)
     end
 
     def password=(password)
-      @password = Password.create(password)
+      if password.empty?
+        @password = ""
+      else
+        @password = Password.create(password)
+      end
       self.password_hash = @password 
     end
   end
