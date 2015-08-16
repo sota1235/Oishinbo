b12f068da409a222d44bbad76bc64f09795fc889
ktarow <ktarou569@gmail.com>
2015-08-16 12:18:40 +0000

    メールアドレスの形式が異なっていた場合のバリデーションを追加

--- models/account.rb
+++ models/account.rb
@@ -11,6 +11,11 @@ module Oishinbo
 
     validates :email, uniqueness: {message: 'このメールアドレスは既に登録されています'}
     validates :email, presence: {message: 'メールアドレスが入力されていません'}
+    validates :email, format: {
+      with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i,
+      on: :create ,
+      message: 'メールアドレスの形式が正しくありません'
+    }
 
     validates :password, presence: {message: 'パスワードが入力されていません'}
     validates :password, confirmation: {message: 'パスワード(確認用)が入力されていません'}
