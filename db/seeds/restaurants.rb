ec39b31ed9743344ddfc965d15f3792707c6b9bc
ktarow <ktarou569@gmail.com>
2015-09-07 22:33:57 +0900

    #51 add: シードデータを追加

--- db/seeds/restaurants.rb
+++ db/seeds/restaurants.rb
@@ -13,3 +13,33 @@ Oishinbo::Restaurant.create(
   created_at: "2015-08-11",
   updated_at: "2015-08-11"
 )
+Oishinbo::Restaurant.create(
+  name: "肉々",
+  name_kana: "ニクニク",
+  address: "東京都渋谷区",
+  category: "カテゴリ",
+  tel: "000-1111-2222",
+  latitude: "30.0",
+  longitube: "40.0",
+  pc_url: "http://nikuniku.com",
+  mobile_url: "http://nikuniku.com/mb",
+  time_detail: "17時",
+  holiday: "木曜日はお休みだよ",
+  created_at: "2015-09-07",
+  updated_at: "2015-09-07"
+)
+Oishinbo::Restaurant.create(
+  name: "魚魚",
+  name_kana: "魚魚",
+  address: "東京都港区",
+  category: "カテゴリ",
+  tel: "000-3333-4444",
+  latitude: "30.0",
+  longitube: "40.0",
+  pc_url: "http://sakana.com",
+  mobile_url: "http://sakana.com/mb",
+  time_detail: "20時",
+  holiday: "金曜日はお休みだよ",
+  created_at: "2015-09-07",
+  updated_at: "2015-09-07"
+)
