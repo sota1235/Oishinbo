229f25e31371e62ff9346c57e4b31256407b90c1
sota1235 <sota1235@gmail.com>
2015-09-08 22:54:37 +0900

    modify: add test method for search_restaurant_by_fw #30

--- spec/unit/services/gurunavi_api_spec.rb
+++ spec/unit/services/gurunavi_api_spec.rb
@@ -1,7 +1,21 @@
 require 'spec_helper'
+require 'rest-client'
 
 RSpec.describe Oishinbo::GurunaviApi do
+  before do
+    @service = Oishinbo::GurunaviApi.new
+  end
+
+  it 'return informations of restaurant by free word searching' do
+    # allow call get function
+    rest_client = RestClient
+    expect(rest_client).to receive(:get).and_return '[ "hoge" ]'
+
+    # assertion
+    expect(@service.search_restaurant_by_fw 'moge', 1).to eq [ "hoge" ]
+  end
+
   it 'return hello' do
-    expect(Oishinbo::GurunaviApi.new.get_restaurant_info).to eq 'hello'
+    expect(@service.get_restaurant_info).to eq 'hello'
   end
 end
