da915a1e6259fa1a6316e544b69ab1debd27a2cd
sota1235 <sota1235@gmail.com>
2015-09-08 23:10:22 +0900

    modify: add method that get information about restaurant by id #30

--- services/gurunavi_api.rb
+++ services/gurunavi_api.rb
@@ -12,28 +12,45 @@ require 'rest-client'
 
 module Oishinbo
   class GurunaviApi
-    Base_host = 'http://api.gnavi.co.jp/'
+    Base_host     = 'http://api.gnavi.co.jp/'
+    RestSearchAPI = 'RestSearchAPI/20150630/'
 
-    def initialize
-      @keyid  = ENV['GURUNAVI_API_KEY']
-      @format = 'json'
+    def initialize(format = 'json')
+      @params = {
+        keyid: ENV['GURUNAVI_API_KEY'],
+        format: 'json'
+      }
     end
 
     # serach restaurant by free word
     def search_restaurant_by_fw(fw, offset = 1)
-      base_url = Base_host + 'RestSearchAPI/20150630/'
+      base_url = Base_host + RestSearchAPI
       freewords = fw.split(' ').join ','
 
       # create request URL
-      params = { keyid: @keyid, freeword: freewords, format: @format, offset: offset }
-      # GET request
-      responce = RestClient.get base_url, {params: params}
-      JSON.parse responce
+      params = @params.merge({
+        freeword: freewords,
+        offset: offset
+      })
+
+      JSON.parse request base_url, params
     end
 
     # get restaurant's information
-    def get_restaurant_info
-      'hello'
+    def get_restaurant_info_by_id(id)
+      base_url = Base_host + RestSearchAPI
+
+      # create request URL
+      params = @params.merge({ id: id })
+
+      JSON.parse request base_url, params
+    end
+
+    private
+
+    # send GET request and return responce
+    def request(base_url, params)
+      RestClient.get base_url, { params: params }
     end
   end
 end
